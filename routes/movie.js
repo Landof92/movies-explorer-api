01d273d40696154212d7b047d16ea8a110d87cca
Angelina Rogacheva <landof@yandex.ru>
2021-11-22 20:43:00 +0300

    изменить число на строку

--- routes/movie.js
+++ routes/movie.js
@@ -1,14 +1,13 @@
-const routerMovie = require('express').Router();
-const { celebrate } = require('celebrate');
-const Joi = require('joi');
-const validateUrl = require('../utils/validateUrl');
+const routerMovie = require("express").Router();
+const { celebrate } = require("celebrate");
+const Joi = require("joi");
+const validateUrl = require("../utils/validateUrl");
 
-const {
-  getMovie, createMovie, deleteMovie,
-} = require('../controllers/movie');
+const { getMovie, createMovie, deleteMovie } = require("../controllers/movie");
 
-routerMovie.get('/movies', getMovie);
-routerMovie.post('/movies',
+routerMovie.get("/movies", getMovie);
+routerMovie.post(
+  "/movies",
   celebrate({
     body: Joi.object().keys({
       country: Joi.string().required(),
@@ -24,11 +23,16 @@ routerMovie.post('/movies',
       movieId: Joi.number().required(),
     }),
   }),
-  createMovie);
-routerMovie.delete('/movies/:movieId', celebrate({
-  params: Joi.object().keys({
-    movieId: Joi.number().required(),
+  createMovie
+);
+routerMovie.delete(
+  "/movies/:movieId",
+  celebrate({
+    params: Joi.object().keys({
+      movieId: Joi.string().hex().length(24),
+    }),
   }),
-}), deleteMovie);
+  deleteMovie
+);
 
 module.exports = routerMovie;
